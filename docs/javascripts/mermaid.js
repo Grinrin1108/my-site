9e935c553742058445b16e4028431ba219f1bf48
Grinrin <126931736+Grinrin1108@users.noreply.github.com>
2026-02-08 16:50:48 +0900

    2026/02/08 16:50

---
+++ docs/javascripts/mermaid.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var config = {
+    startOnLoad: true,
+    theme: "default",
+    securityLevel: "loose",
+  };
+  mermaid.initialize(config);
+});
